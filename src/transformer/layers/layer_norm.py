47f5ecee96ba69db51b262bc8a01a5171601ded3
evanzyfan <evanzyfan@tencent.com>
2026-08-13 15:46:02 +0800

    implement Layer Normalization layer

--- src/transformer/layers/layer_norm.py
+++ src/transformer/layers/layer_norm.py
@@ -7,7 +7,13 @@ class LayerNorm(nn.Module):
 
     def __init__(self, d_model: int, eps: float = 1e-12):
         super(LayerNorm, self).__init__()
-        raise NotImplementedError
+        self.gamma = nn.Parameter(torch.ones(d_model))
+        self.beta = nn.Parameter(torch.zeros(d_model))
+        self.eps = eps
 
     def forward(self, x: torch.Tensor):
-        raise NotImplementedError
+        mean = x.mean(dim=-1, keepdim=True)
+        var = x.var(dim=-1, keepdim=True)
+        z = (x - mean) / (var + self.eps)
+
+        return self.gamma * z + self.beta
