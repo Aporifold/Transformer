fix a bug of attention mask

src/transformer/layers/scale_dot_product_attention.py
@@ -7,14 +7,21 @@ from torch.nn import functional as F
 class ScaleDotProductAttention(nn.Module):
     """Scaled-Dot Product Attention."""
 
-    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask=None):
+    def forward(
+        self,
+        q: torch.Tensor,
+        k: torch.Tensor,
+        v: torch.Tensor,
+        mask: torch.Tensor | None = None,
+    ):
         """Apply Scaled-Dot Product Attention.
 
         Args:
             q (torch.Tensor): Query tensor, of shape (B, H, L, D)
             k (torch.Tensor): Key tensor, of shape (B, H, L, D)
             v (torch.Tensor): Value tensor, of shape (B, H, L, D)
-            mask (torch.Tensor, optional): Attention mask. Defaults to None.
+            mask (torch.Tensor, optional): Boolean attention mask, 
+                of shape (B, 1, 1, L). Defaults to None.
 
         Returns:
             tuple (torch.Tensor, torch.Tensor): A tuple of attention outputs and attention weights
@@ -23,7 +30,7 @@ class ScaleDotProductAttention(nn.Module):
         scores: torch.Tensor = q @ k.transpose(-1, -2) * d_k**-0.5
 
         if mask is not None:
-            scores.masked_fill(mask == 0, float("-inf"))
+            scores.masked_fill(mask, float("-inf"))
 
         attn_weights = F.softmax(scores, dim=-1)
 